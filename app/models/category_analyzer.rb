39340166893652e08e65424ddad2506747abaa3b
Ahmad hamza <ahmadhamza19@gmail.com>
2024-03-31 21:25:36 +0530

    few more corrections

--- app/models/category_analyzer.rb
+++ app/models/category_analyzer.rb
@@ -9,12 +9,12 @@ class CategoryAnalyzer
     @data.each do |category, sub_categories|
       total[category] = {}
       if category == "others"
-        total[category] = sub_categories.sum {|t| t["Debit Amount"].to_f}
+        total[category] = sub_categories.sum {|t| t["Debit Amount"].to_f}.round(2)
       else
         sub_categories.each do |sub_category, transactions|
           next unless transactions
           
-          total[category][sub_category] = transactions.sum {|t| t["Debit Amount"].to_f}
+          total[category][sub_category] = transactions.sum {|t| t["Debit Amount"].to_f}.round(2)
         end
       end
     end
@@ -28,9 +28,9 @@ class CategoryAnalyzer
       if category == "others"
         result[category] = sub_categories
       else
-        result[category] = sub_categories.sum {|(k, v)| v}
+        result[category] = sub_categories.sum {|(k, v)| v}.round(2)
       end
     end
-    result
+    result.sort_by {|k, v| v}.reverse.to_h
   end
 end
